b66950ae6baa030e1613cd6badb7847feae50925
Ayshath_Shaima <ayshathshaima119@gmail.com>
2025-08-19 12:54:46 -0700

    Modified

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,31 +1,29 @@
 import ProductCard from "@/app/Components/ProductCard/CardPage";
 import { ProductServices } from "@/app/services/products-services";
 
-type PageProp = {
+type CategoryPageProps = {
   params: {
     category: string;
   };
-}
+};
 
-export default async function CategoryPage({ params }: PageProp) {
+export default async function CategoryPage({ params }: CategoryPageProps) {
   const categoryName = decodeURIComponent(params.category);
 
   const data = await ProductServices.getProductByCategory(categoryName);
-
   const products = data?.products ?? [];
 
-  
-return (
-  <div className="min-h-screen bg-yellow-50 py-8 px-4">
-    <h1 className="text-3xl font-extrabold italic bg-amber-300 mb-8 text-center text-black">
-      Category: {categoryName}
-    </h1>
+  return (
+    <div className="min-h-screen bg-yellow-50 py-8 px-4">
+      <h1 className="text-3xl font-extrabold italic bg-amber-300 mb-8 text-center text-black">
+        Category: {categoryName}
+      </h1>
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-      {products.map((product: any) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        {products.map((product: any) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
